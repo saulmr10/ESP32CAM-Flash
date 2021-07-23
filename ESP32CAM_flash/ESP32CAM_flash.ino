13b42b4cb635be3a79a8136c6344f4778b9159a8
saulmr10 <toup1213@gmail.com>
2021-07-23 12:26:31 -0500

    Cambio en el pin del LED
    Cambio del pin 4 por el 5

--- ESP32CAM_flash/ESP32CAM_flash.ino
+++ ESP32CAM_flash/ESP32CAM_flash.ino
@@ -8,7 +8,7 @@
 //hecho de que cuentas con la base programadora.
 
 //Definición de pines
-#define LAMP 4 //Se selecciona 
+#define LAMP 5 //Se selecciona 
 
 //Inicialización del programa
 void setup() //Estructura principal
